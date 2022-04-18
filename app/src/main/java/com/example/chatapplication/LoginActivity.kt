b497869c7858858912914a9a27c87ca8f8423bdb
kiarie404 <kiarie404@gmail.com>
2022-04-18 08:22:12 -0700

    I have fixed further unresolved references, turns out declaring variables first fixes the issue As for the line : 8, Let's just leave it that way

--- app/src/main/java/com/example/chatapplication/LoginActivity.kt
+++ app/src/main/java/com/example/chatapplication/LoginActivity.kt
@@ -2,8 +2,10 @@ package com.example.chatapplication
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_log_in.*
+import kotlinx.android.synthetic.main.activity_log_in.*   // I could not fix this error
 
 class LoginActivity: AppCompatActivity() {
 
@@ -11,12 +13,21 @@ class LoginActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
 
-        signin_button.setOnClickListener {
-            val email = email_editText.text.toString()
-            val password = password_editText.text.toString()
+        // created a virtual variables instead... to fix unresolved references  --- delete line if neccessary
+        val signin_button = findViewById<Button>(R.id.signup_button)
+        val login_email = findViewById<TextView>(R.id.email_editText)
+        val login_password = findViewById<TextView>(R.id.password_editText)
+        val signup_text = findViewById<TextView>(R.id.signup_text)
 
-            Log.d("LoginActivity", "Email is: $email")
-            Log.d("LoginActivity", "Password: $password")
+
+
+        signin_button.setOnClickListener {
+
+            val email_as_string = login_email.text.toString()
+            val password_as_string = login_password.text.toString()
+
+            Log.d("LoginActivity", "Email is: $email_as_string")
+            Log.d("LoginActivity", "Password: $password_as_string")
 
             // Firebase Authentication to create a user with email and password
         }
