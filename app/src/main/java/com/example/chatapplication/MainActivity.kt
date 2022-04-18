a817eca48f220241f7d3ba21b9564317d60534ef
kiarie404 <kiarie404@gmail.com>
2022-04-18 08:31:58 -0700

    Same issues fixed. but for the MainActivity.kt

--- app/src/main/java/com/example/chatapplication/MainActivity.kt
+++ app/src/main/java/com/example/chatapplication/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.chatapplication
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,10 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // created a virtual variables instead... to fix unresolved references  --- delete line if neccessary
+        val signup_button = findViewById<Button>(R.id.signup_button)
+        val signin_text = findViewById<TextView>(R.id.signin_text)
+
         signup_button.setOnClickListener {
-            val username = username_editText.text.toString()
-            val email = email_editText.text.toString()
-            val password = password_editText.text.toString()
+            val username = findViewById<TextView>(R.id.username_editText)
+            val email = findViewById<TextView>(R.id.email_editText)
+            val password = findViewById<TextView>(R.id.password_editText)
 
             Log.d("MainActivity", "Username is: $username")
             Log.d("MainActivity", "Email is: $email")
