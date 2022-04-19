d0fdabff75b8705033e1b5397cd67cc35e752725
kiarie404 <kiarie404@gmail.com>
2022-04-19 00:12:38 -0700

    created a new function perform_signup, This is just for the sake of modularity

--- app/src/main/java/com/example/chatapplication/MainActivity.kt
+++ app/src/main/java/com/example/chatapplication/MainActivity.kt
@@ -21,39 +21,8 @@ class MainActivity : AppCompatActivity() {
         val signin_text = findViewById<TextView>(R.id.signin_text)
 
         signup_button.setOnClickListener {
-            val username = findViewById<TextView>(R.id.username_editText)
-            val email = findViewById<TextView>(R.id.email_editText)
-            val password = findViewById<TextView>(R.id.password_editText)
 
-            // converting input variables to strings... for easier manipulation in logCats and passing to third_party functions
-            val username_as_str = username.text.toString()
-            val email_as_str = email.text.toString()
-            val password_as_str = password.text.toString()
-
-            // to avoid the user pressing the signup button with empty inputs...
-            if (email_as_str.isEmpty() || password_as_str.isEmpty()){
-                Toast.makeText(this, "Please enter a text in email and password", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-
-            Log.d("MainActivity", "Username is: $username_as_str")
-            Log.d("MainActivity", "Email is: $email_as_str")
-            Log.d("MainActivity", "Password: $password_as_str")
-
-            // Firebase Authentication to create a user with email and password
-            // createing a new user using email and password
-            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email_as_str, password_as_str)
-                // if user is done entering details...
-                .addOnCompleteListener{
-                    if(!it.isSuccessful) return@addOnCompleteListener
-
-                    // else, if unsuccessful
-                    Log.d("Main", "Successfully created a user using uid : ${it.result.user?.uid}") // the question feature was a suggestion from that red-bulb suggestion thing
-                }
-                .addOnFailureListener {
-                    Log.d("main", "Failed to create new user : ${it.message}")
-                }
-
+            perform_signup()
         }
 
         signin_text.setOnClickListener {
@@ -64,4 +33,41 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun perform_signup(){
+        // extract IDs from the related layout
+        val username = findViewById<TextView>(R.id.username_editText)
+        val email = findViewById<TextView>(R.id.email_editText)
+        val password = findViewById<TextView>(R.id.password_editText)
+
+        // converting input variables to strings... for easier manipulation in logCats and passing to third_party functions
+        val username_as_str = username.text.toString()
+        val email_as_str = email.text.toString()
+        val password_as_str = password.text.toString()
+
+        // to avoid the user pressing the signup button with empty inputs...
+        if (email_as_str.isEmpty() || password_as_str.isEmpty()){
+            Toast.makeText(this, "Please enter a text in email and password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("MainActivity", "Username is: $username_as_str")
+        Log.d("MainActivity", "Email is: $email_as_str")
+        Log.d("MainActivity", "Password: $password_as_str")
+
+        // Firebase Authentication to create a user with email and password
+        // createing a new user using email and password
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email_as_str, password_as_str)
+            // if user is done entering details...
+            .addOnCompleteListener{
+                if(!it.isSuccessful) return@addOnCompleteListener
+
+                // else, if unsuccessful
+                Log.d("Main", "Successfully created a user using uid : ${it.result.user?.uid}") // the question feature was a suggestion from that red-bulb suggestion thing
+            }
+            .addOnFailureListener {
+                Log.d("main", "Failed to create new user : ${it.message}")
+            }
+    }
+
 }
