5b5d6ef1e67fd0640d394dba47ed59405b2e79bd
kiarie404 <kiarie404@gmail.com>
2022-04-18 23:20:56 -0700

    Impleneted Firebase Authentication during the signup stage

--- app/src/main/java/com/example/chatapplication/MainActivity.kt
+++ app/src/main/java/com/example/chatapplication/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -23,11 +25,35 @@ class MainActivity : AppCompatActivity() {
             val email = findViewById<TextView>(R.id.email_editText)
             val password = findViewById<TextView>(R.id.password_editText)
 
-            Log.d("MainActivity", "Username is: $username")
-            Log.d("MainActivity", "Email is: $email")
-            Log.d("MainActivity", "Password: $password")
+            // converting input variables to strings... for easier manipulation in logCats and passing to third_party functions
+            val username_as_str = username.text.toString()
+            val email_as_str = email.text.toString()
+            val password_as_str = password.text.toString()
+
+            // to avoid the user pressing the signup button with empty inputs...
+            if (email_as_str.isEmpty() || password_as_str.isEmpty()){
+                Toast.makeText(this, "Please enter a text in email and password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            Log.d("MainActivity", "Username is: $username_as_str")
+            Log.d("MainActivity", "Email is: $email_as_str")
+            Log.d("MainActivity", "Password: $password_as_str")
 
             // Firebase Authentication to create a user with email and password
+            // createing a new user using email and password
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email_as_str, password_as_str)
+                // if user is done entering details...
+                .addOnCompleteListener{
+                    if(!it.isSuccessful) return@addOnCompleteListener
+
+                    // else, if unsuccessful
+                    Log.d("Main", "Successfully created a user using uid : ${it.result.user?.uid}") // the question feature was a suggestion from that red-bulb suggestion thing
+                }
+                .addOnFailureListener {
+                    Log.d("main", "Failed to create new user : ${it.message}")
+                }
+
         }
 
         signin_text.setOnClickListener {
